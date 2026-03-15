004a25a14ac5d1c2583e82d5d1a9f83e6626b720
de-yarp <volodymyr.antonov@student.tuke.sk>
2026-03-15 12:17:56 +0100

    feat: MFCC_CNN now has dynamic config-dependent structure (#15)

--- models/cnn_mfcc.py
+++ models/cnn_mfcc.py
@@ -1,51 +1,103 @@
+from math import floor
+
 import torch
 import torch.nn.functional as F
 import torch.nn.modules as nn
 
-from infra.data_models import ConfigCNN
+from infra.data_models import ConfigCNN, LayerConv, LayerPool
 
 
 class MFCC_CNN(nn.Module):
     def __init__(self, *, cfg: ConfigCNN):
         super().__init__()
 
-        self.conv1 = nn.Conv2d(  # (1, 120, 216)
-            1,
-            cfg.conv_kernel_count,
-            cfg.conv_kernel_size,
-            cfg.conv_stride,
-            cfg.conv_padding,
-        )
-        self.pool = nn.MaxPool2d(  # (16, 118, 214)
-            cfg.pool_kernel_size, cfg.pool_stride, cfg.pool_padding
-        )
-        self.conv2 = nn.Conv2d(  # (16, 59, 107)
-            cfg.conv_kernel_count,
-            2 * cfg.conv_kernel_count,
-            cfg.conv_kernel_size,
-            cfg.conv_stride,
-            cfg.conv_padding,
-        )  # (32, 57, 105)
-        # (32, 28, 52)
-        self.fc1 = nn.Linear(32 * 28 * 52, 512)
-        self.fc2 = nn.Linear(512, 128)
-        self.fc3 = nn.Linear(128, 50)
+        self.activation_fn = F.relu
+        self.num_classes = cfg.num_classes
+        self.conv_layers_list = nn.ModuleList()
+        self.fc_layers_list = nn.ModuleList()
+        out_channels = 1
+        height = 120
+        width = 216
+        for layer in cfg.conv_layers:
+            kernel_count = -1
+            if isinstance(layer, LayerConv):
+                self.conv_layers_list.append(
+                    nn.Conv2d(
+                        out_channels,
+                        layer.kernel_count,
+                        layer.kernel_size,
+                        layer.stride,
+                        layer.padding,
+                    )
+                )
+                if layer.batch_norm:
+                    self.conv_layers_list.append(nn.BatchNorm2d(layer.kernel_count))
+
+                kernel_count = layer.kernel_count
+            if isinstance(layer, LayerPool):
+                self.conv_layers_list.append(
+                    nn.MaxPool2d(
+                        layer.kernel_size,
+                        layer.stride,
+                        layer.padding,
+                    )
+                )
+                kernel_count = out_channels
+
+            out_channels, height, width = self._post_transform_shape(
+                kernel_count,
+                layer.kernel_size,
+                layer.stride,
+                layer.padding,
+                width,
+                height,
+            )
+
+        flat_tensor_shape = out_channels * height * width
+        fc_layers = cfg.fc_layers
+
+        last_fc_out_channels = fc_layers[0]
+        self.fc_layers_list.append(nn.Linear(flat_tensor_shape, last_fc_out_channels))
+        for fc_out_channels in fc_layers[1:]:
+            self.fc_layers_list.append(nn.Linear(last_fc_out_channels, fc_out_channels))
+            last_fc_out_channels = fc_out_channels
+        self.fc_layers_list.append(nn.Linear(last_fc_out_channels, self.num_classes))
+
+    def _post_transform_shape(
+        self,
+        out_channels: int,
+        kernel_size: int,
+        stride: int,
+        padding: int,
+        width: int,
+        height: int,
+    ):
+        new_height = floor((height - kernel_size + 2 * padding) / stride) + 1
+        new_width = floor((width - kernel_size + 2 * padding) / stride) + 1
+        return (out_channels, new_height, new_width)
 
     def forward(self, x: torch.Tensor):
+        def _is_next_batch_norm(conv_layer_list: nn.ModuleList, idx: int) -> bool:
+            try:
+                return isinstance(conv_layer_list[idx + 1], nn.BatchNorm2d)
+            except IndexError:
+                return False
+
         x = x.unsqueeze(1)
-        x = self.conv1(x)
-        x = F.relu(x)
-        x = self.pool(x)
-        x = self.conv2(x)
-        x = F.relu(x)
-        x = self.pool(x)
+        for idx, conv in enumerate(self.conv_layers_list):
+            # conv with batch norm next → no relu. conv without batch norm next → relu. batch norm → relu (falls through to last line). pool → no relu
+            if isinstance(conv, nn.MaxPool2d) or _is_next_batch_norm(
+                self.conv_layers_list, idx
+            ):
+                x = conv(x)
+                continue
+            x = self.activation_fn(conv(x))
 
         x = torch.flatten(x, 1)
 
-        x = self.fc1(x)
-        x = F.relu(x)
-        x = self.fc2(x)
-        x = F.relu(x)
-        x = self.fc3(x)
+        for fc in self.fc_layers_list[:-1]:
+            x = self.activation_fn(fc(x))
+
+        x = self.fc_layers_list[-1](x)
 
         return x
